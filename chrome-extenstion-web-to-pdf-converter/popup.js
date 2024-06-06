aafc1d0a7706c1bbe72869b08749ca338c97823e
Abhi Sharma <161134765+abbysheksharma@users.noreply.github.com>
2024-06-06 17:50:13 +0530

    Delete chrome-extenstion-web-to-pdf-converter directory

--- chrome-extenstion-web-to-pdf-converter/popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("convertButton").addEventListener("click", () => {
-  chrome.runtime.sendMessage({ action: "convertToPDF" }, (response) => {
-    console.log(response.status);
-  });
-});
